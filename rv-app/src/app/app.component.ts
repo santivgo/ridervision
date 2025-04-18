fbcb59bff0d89f91b2c2234303b2967f099e53ec
veqis <alyssonhenrique000@gmail.com>
2025-04-18 16:34:36 -0300

    adicionado efeitos gradient na tela de conteudo

--- rv-app/src/app/app.component.ts
+++ rv-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
