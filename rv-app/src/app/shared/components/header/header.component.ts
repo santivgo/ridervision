a8ebdcd980471a1ea87a6ed25d5a88320f49a9ae
santivgo <requiemsantiago@gmail.com>
2025-04-19 13:31:09 -0300

    clique funcionando

--- rv-app/src/app/shared/components/header/header.component.ts
+++ rv-app/src/app/shared/components/header/header.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderMenuButtonComponent } from './header-menu-button/header-menu-button.component';
 import { HorizontalSepComponent } from '../horizontal-sep/horizontal-sep.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [HeaderMenuButtonComponent, HorizontalSepComponent],
+  imports: [CommonModule, HeaderMenuButtonComponent, HorizontalSepComponent, ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
 export class HeaderComponent {
-
+  selectedButton: string = ''
+  onButtonClicked(event: MouseEvent){
+    this.selectedButton = (event.target as HTMLDivElement).innerText 
+  } 
 }
