010db9d19113a39a20b0b2047e4469da11a00ceb
veqis <alyssonhenrique000@gmail.com>
2025-04-19 10:54:01 -0300

    push só para dar sync com os card de comentario qu o sant fez

--- rv-app/src/app/app.component.ts
+++ rv-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { CardRiderComponent } from './shared/components/main-cards/card-rider/card-rider.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardRiderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
---
+++ rv-app/src/app/shared/components/main-cards/card-rider/card-rider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-rider',
+  imports: [],
+  templateUrl: './card-rider.component.html',
+  styleUrl: './card-rider.component.sass'
+})
+export class CardRiderComponent {
+
+}
