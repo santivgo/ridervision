d77db67277628acf3ae08147dc1dc8a8bfa9bb7b
veqis <alyssonhenrique000@gmail.com>
2025-04-19 21:56:30 -0300

    mais umas coisas pra tela inicial

--- rv-app/src/app/app.component.ts
+++ rv-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { CommentComponent } from './shared/components/comment/comment.component';
+import { CardRiderComponent } from './shared/components/main-cards/card-rider/card-rider.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CommentComponent],
+  imports: [HeaderComponent, CommentComponent, CardRiderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- rv-app/src/app/shared/components/main-cards/card-rider/card-rider.component.ts
+++ rv-app/src/app/shared/components/main-cards/card-rider/card-rider.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './card-rider.component.sass'
 })
 export class CardRiderComponent {
-
+  imgSrc: string = '/assets/MiBRrBm.png';
+  riderName: string = 'MAJEDE';
+  riderDescription: string = 'from kamen rider gotchard';
+  textAreaTitle: string = 'BASE FORM';
+  textAreaContent: string = `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`;
 }
