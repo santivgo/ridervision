faba08dd3ca5637041a820523fde108174856b13
veqis <alyssonhenrique000@gmail.com>
2025-04-29 11:20:15 -0300

    implementada caixa de comentarios com leitura da api simulada com json server

---
+++ rv-app/src/app/shared/components/comment-input/comment-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-input',
+  imports: [],
+  templateUrl: './comment-input.component.html',
+  styleUrl: './comment-input.component.sass'
+})
+export class CommentInputComponent {
+
+}
---
+++ rv-app/src/app/shared/components/comment/comment.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  private apiUrl = 'http://localhost:3000/comments'; 
+  // aqui é o url do json server
+  //json-server --watch db.json --port 3000
+
+  constructor(private http: HttpClient) {}
+
+  getComments(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
