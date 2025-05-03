0bbacf48a0ea4eea74d7e0c8669c0509192ed372
santivgo <requiemsantiago@gmail.com>
2025-05-03 13:20:23 -0300

    json server api

--- rv-app/src/app/shared/components/showcase/showcase-card/directives/showcase-card-img.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: 'app-showcase-img, [appShowcaseImg]',
-  host: {'class': 'rv-c-showcase-card__header__img'}
-})
-export class ShowcaseCardImgDirective {
-
-}
--- scrappers/main_show.py
+++
@@ -1,7 +0,0 @@
-class kamenRiderSeries:
-    def __init__(self, id, name, year, poster, synopsis):
-        self.id = id
-        self.name = name
-        self.year = year
-        self.synopsis = synopsis
-        self.imgs = {'rider_img_xl': poster, 'rider_img_bg': ''}
