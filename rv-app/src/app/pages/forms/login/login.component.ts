3b5f0f4d9606460dd8891d19060c0264d303b8f7
santivgo <requiemsantiago@gmail.com>
2025-05-09 16:54:24 -0300

    cadastro

--- rv-app/src/app/pages/forms/login/login.component.ts
+++ rv-app/src/app/pages/forms/login/login.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { Router, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatFormFieldModule, MatInputModule, RouterOutlet, RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
+  constructor(private router: Router){}
 
 }
