200e00995060dfcad2825e594e92c3cfceb611a9
santivgo <requiemsantiago@gmail.com>
2025-05-09 09:21:45 -0300

    adicionada a rota pra login e cadastro, adicionados no menu e agora o sass está adaptado para min height 100vh

--- rv-app/src/app/app.routes.ts
+++ rv-app/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainCardRiderComponent } from './pages/main-card-rider/main-card-rider.component';
 import { ShowcaseRiderComponent } from './pages/showcase-rider/showcase-rider.component';
+import { LoginComponent } from './pages/forms/login/login.component';
+import { CadastroComponent } from './pages/forms/cadastro/cadastro.component';
 
 export const routes: Routes = [
     {path: '', component: MainCardRiderComponent},
-    {path: 'riders', component: ShowcaseRiderComponent}
+    {path: 'riders', component: ShowcaseRiderComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'cadastro', component: CadastroComponent},
+
 
 ];
