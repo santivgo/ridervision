39dbd33f48cf788e91137b9b4bb46bd57cf907ba
santivgo <requiemsantiago@gmail.com>
2025-05-09 19:52:49 -0300

    favicon e arrumando a rota

--- rv-app/src/app/pages/forms/login/login.component.ts
+++ rv-app/src/app/pages/forms/login/login.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { Router, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormFieldModule, MatInputModule, RouterOutlet, RouterLinkActive],
+  imports: [MatFormFieldModule, MatInputModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
