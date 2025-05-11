0be5b6998dd740565b88a013e6c8e5ef9802e010
veqis <alyssonhenrique000@gmail.com>
2025-05-11 10:03:28 -0300

    implementada funcionalidade de leitura de api e exibitção para series

--- rv-app/src/app/pages/profile/profile.component.ts
+++ rv-app/src/app/pages/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { CommentComponent } from '../../shared/components/comment/comment.compon
 import { CardHeaderDirective } from '../../core/directives/card-header.directive';
 import { ButtonIconComponent } from '../../shared/components/showcase/showcase-line/button-icon/button-icon.component';
 import { CommonModule } from '@angular/common';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommentService } from '../../core/services/comment.service';
 import { IShow } from '../../core/interfaces/show';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
@@ -11,44 +12,52 @@ import { SeriesService } from '../../core/services/series.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [DividerHorizontalComponent, CommentComponent, CommonModule, CardHeaderDirective, ButtonIconComponent],
+  imports: [DividerHorizontalComponent, CommentComponent, CommonModule, CardHeaderDirective, ButtonIconComponent, NgbCollapseModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.sass'
 })
 export class ProfileComponent {
+    @Input({'alias': 'collapseRef', required: true}) collapse!: NgbCollapse;
+    comments: any[] = [];
+
+    imgSrc: string = '';
+    seriesName: string = '';
+
+    @Input({'required': true}) riders: IShow[] = [];
     riderCollapse: IShow = {} as IShow;
     isCollapsed: boolean = true;
-    imgSrc: string = '/assets/MiBRrBm.png';
-    riderName: string = 'MAJEDE';
-    collapse!: NgbCollapse;
-    intervalId: any;
-
-    comments: any[] = [];
-    @Input({ required: true }) riders: IShow[] = [];
 
     constructor(private commentsService: CommentService, private series: SeriesService) {}
-    
 
     ngOnInit(): void {
       this.getComments();
       this.getSeries();
     }
 
-    getComments(): void{ // por agora mockado, mas depois usar bd para pegar os comentarios da conta
+    getComments(): void {
       this.commentsService.getComments().subscribe((data) => {
         this.comments = data;
       });
     }
 
-    getSeries(): void{
+    getSeries(): void {
       this.series.getShows().subscribe((data) => {
-  
-        this.riders = data
-  
+        this.riders = data;
+        this.selectRandomRider();
       });
     }
 
-    changeRiderCollapse(rider: IShow){
+    selectRandomRider(): void {
+      if (this.riders.length > 0) {
+        const randomIndex = Math.floor(Math.random() * this.riders.length);
+        this.changeRiderCollapse(this.riders[randomIndex]);
+        this.isCollapsed = false;
+      }
+    }
+
+    changeRiderCollapse(rider: IShow): void {
       this.riderCollapse = rider;
+      this.imgSrc = rider.imgs.rider_img_xl; // Temporariamente usando essa imagem enquanto o banco não está com as imagens PNG de poses
+      this.seriesName = rider.name.replace('Kamen Rider ', '').replace(' (TV Show)', '');
     }
 }
