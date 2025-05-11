3771856270da110326dbeee48be1bd6993bac451
veqis <alyssonhenrique000@gmail.com>
2025-05-11 17:43:54 -0300

    ajustes finais tela perfil

--- rv-app/src/app/pages/profile/profile.component.ts
+++ rv-app/src/app/pages/profile/profile.component.ts
@@ -1,63 +1,76 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbCollapse, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { IShow } from '../../core/interfaces/show';
+import { CommentService } from '../../core/services/comment.service';
+import { SeriesService } from '../../core/services/series.service';
+
 import { DividerHorizontalComponent } from '../../shared/components/dividers/divider-horizontal/divider-horizontal.component';
 import { CommentComponent } from '../../shared/components/comment/comment.component';
 import { CardHeaderDirective } from '../../core/directives/card-header.directive';
 import { ButtonIconComponent } from '../../shared/components/showcase/showcase-line/button-icon/button-icon.component';
-import { CommonModule } from '@angular/common';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { CommentService } from '../../core/services/comment.service';
-import { IShow } from '../../core/interfaces/show';
-import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
-import { SeriesService } from '../../core/services/series.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [DividerHorizontalComponent, CommentComponent, CommonModule, CardHeaderDirective, ButtonIconComponent, NgbCollapseModule],
+  standalone: true,
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.sass'
+  styleUrl: './profile.component.sass',
+  imports: [
+    CommonModule,
+    NgbCollapseModule,
+    DividerHorizontalComponent,
+    CommentComponent,
+    CardHeaderDirective,
+    ButtonIconComponent,
+  ]
 })
 export class ProfileComponent {
-    @Input({'alias': 'collapseRef', required: true}) collapse!: NgbCollapse;
-    comments: any[] = [];
+  @Input({'alias': 'collapseRef', required: true}) collapse!: NgbCollapse;
+  @Input() riders: IShow[] = [];
 
-    imgSrc: string = '';
-    seriesName: string = '';
+  imgSrc: string = '';
+  seriesName: string = '';
+  isCollapsed: boolean = true;
+  riderCollapse: IShow = {} as IShow;
+  comments: any[] = [];
 
-    @Input({'required': true}) riders: IShow[] = [];
-    riderCollapse: IShow = {} as IShow;
-    isCollapsed: boolean = true;
+  constructor(
+    private commentsService: CommentService,
+    private seriesService: SeriesService
+  ) {}
 
-    constructor(private commentsService: CommentService, private series: SeriesService) {}
+  ngOnInit(): void {
+    this.loadComments();
+    this.loadSeries();
+  }
 
-    ngOnInit(): void {
-      this.getComments();
-      this.getSeries();
+  private loadComments(): void {
+    this.commentsService.getComments().subscribe((data) => {
+      this.comments = data;
+    });
+  }
+
+  private loadSeries(): void {
+    this.seriesService.getShows().subscribe((data) => {
+      this.riders = data;
+      this.setRandomRider();
+    });
+  }
+
+  private setRandomRider(): void {
+    if (this.riders.length) {
+      const index = Math.floor(Math.random() * this.riders.length);
+      this.selectRider(this.riders[index]);
+      this.isCollapsed = false;
     }
+  }
 
-    getComments(): void {
-      this.commentsService.getComments().subscribe((data) => {
-        this.comments = data;
-      });
-    }
+  selectRider(rider: IShow): void {
+    this.riderCollapse = rider;
+    this.imgSrc = rider.imgs.rider_img_xl; // Temporariamente usando essa imagem enquanto o banco não está com as imagens PNG de poses :v
+    this.seriesName = rider.name.replace('Kamen Rider ', '').replace(' (TV Show)', '');
+  }
 
-    getSeries(): void {
-      this.series.getShows().subscribe((data) => {
-        this.riders = data;
-        this.selectRandomRider();
-      });
-    }
 
-    selectRandomRider(): void {
-      if (this.riders.length > 0) {
-        const randomIndex = Math.floor(Math.random() * this.riders.length);
-        this.changeRiderCollapse(this.riders[randomIndex]);
-        this.isCollapsed = false;
-      }
-    }
-
-    changeRiderCollapse(rider: IShow): void {
-      this.riderCollapse = rider;
-      this.imgSrc = rider.imgs.rider_img_xl; // Temporariamente usando essa imagem enquanto o banco não está com as imagens PNG de poses
-      this.seriesName = rider.name.replace('Kamen Rider ', '').replace(' (TV Show)', '');
-    }
 }
