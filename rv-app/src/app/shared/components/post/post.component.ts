0ffbdd4b3802b9d8585f6c7dfcb2f18334f3d742
veqis <alyssonhenrique000@gmail.com>
2025-05-29 11:22:50 -0300

    implementado compoente de comentario para post no perfil com funcionalidade de mudar visibilidade

--- rv-app/src/app/shared/components/post/post.component.ts
+++ rv-app/src/app/shared/components/post/post.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TagComponent } from '../tag/tag.component';
 import { IUser } from '../../../core/interfaces/models/user.interface';
 import { IPost } from '../../../core/interfaces/models/post.interface';
+import { PostCommentComponent } from './post-comment/post-comment.component';
 
 @Component({
   selector: 'app-post',
-  imports: [TagComponent, CommonModule],
+  imports: [TagComponent, CommonModule, PostCommentComponent],
   templateUrl: './post.component.html',
   styleUrl: './post.component.sass'
 })
@@ -14,4 +15,10 @@ export class PostComponent {
   @Input({required: true})
   user: IUser = {} as IUser
   post: IPost = {} as IPost
+
+  exibirComments: boolean = false
+
+  toggleComments(): void {
+    this.exibirComments = !this.exibirComments
+  }
 }
