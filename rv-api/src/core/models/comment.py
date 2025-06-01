3fbbe7bf8e77d1f5b347c7e3251088e94d5dd2f5
santivgo <requiemsantiago@gmail.com>
2025-06-01 00:11:22 -0300

    banco funcionando v1

--- rv-api/src/core/models/comment.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from core.models.user import User
-from core.models.post import Post
-
-class Comment(models.Model):
-    content = models.CharField(blank=False, null=False)
-    date = models.DateField(blank=False, null=False)
-    author = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- rv-api/src/core/views.py
+++ rv-api/src/core/views.py
@@ -1,1 +1,26 @@
-from core.views import *
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from core.serializers import RiderSerializer, ShowSerializer, ShowBulkSerializer
+from core.models import Rider, Show
+
+class RiderView(viewsets.ModelViewSet):
+    queryset = Rider.objects.all()
+    serializer_class = RiderSerializer
+    def create(self, request, *args, **kwargs):
+        many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
+
+class ShowView(viewsets.ModelViewSet):
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
+
+    def create(self, request, *args, **kwargs):
+        many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
