a1da24576f4580bcfc92bfd53d2d5d28de777dd1
santivgo <requiemsantiago@gmail.com>
2025-06-06 09:57:28 -0300

    Criação do View e Serializador de Review e User (closes #38, closes #33)

--- rv-api/src/core/serializers.py
+++ rv-api/src/core/serializers.py
@@ -1,31 +1,25 @@
 from rest_framework import serializers
-from core.models import Rider, Show
+from core.models import Rider, Show, User, Review
 from django.conf import settings
 from django.core.files import File
 import os
 
-class RiderBulkSerializer(serializers.ListSerializer):
-    def create(self, validated_data):
-        obj = [Rider(**item) for item in validated_data]
-        return Rider.objects.bulk_create(obj)
-    
-class RiderSerializer(serializers.ModelSerializer):
+class RiderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Rider
         fields = "__all__"
-        list_serializer_class = RiderBulkSerializer
 
-class ShowBulkSerializer(serializers.ListSerializer):
-
-    def create(self, validated_data):
-        obj = [Show(**item) for item in validated_data]
-        return Show.objects.bulk_create(obj)
-
-class ShowSerializer(serializers.ModelSerializer):
+class ShowSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Show
         fields = "__all__"
-        list_serializer_class = ShowBulkSerializer
-    
-   
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'img']
+   
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
